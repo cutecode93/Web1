272a149d77b224c328e75bdaddfea882ed2cac7d
Angela <sonayin61@gmail.com>
2021-04-30 08:03:12 +0900

    Jaksim3daysX666

--- starter/script10.js
+++ starter/script10.js
@@ -107,7 +107,7 @@ lolol
 lol`)
 */
 
-
+/*
 //18강
 const age = 30;
 const isOldEnough = age >= 18;
@@ -136,3 +136,119 @@ if (birthYear <= 2000) {
     century = 21;
 }
 console.log(century);
+*/
+
+
+//20강
+
+//conversion
+const inputYear = '1991';
+console.log(Number(inputYear), inputYear);
+console.log(Number(inputYear) + 18);
+
+console.log(Number('Jonas'));
+console.log(typeof NaN);
+
+console.log(String(23), 23);
+
+//coercion
+console.log('I am ' + 23 + ' years old'); //=
+console.log('I am ' + '23' + ' years old');
+
+console.log('23' + '10' + 3); // =23103.  + 연산자에서만 강제변환이 일어나지 않음!! (중요) 문자열 +기능 때문인듯
+console.log('23' - '10' - 3); // = 10. 강제변환됨
+console.log('23' * '2');
+console.log('23' > '18'); // =true. 이유는? string인 이것을 number로 js가 강제변환 시켜버리기 때문
+
+//quiz
+let n = '1' + 1; // =11
+n = n - 1; // =10
+console.log(n);
+
+
+
+
+
+//21강
+
+//falsy values: 0, '', undefined, null, NaN
+
+console.log(Boolean(0));
+console.log(Boolean(undefined));
+console.log(Boolean('Jonas'));
+console.log(Boolean({})); // = empty object -> true
+console.log(Boolean(''));
+
+const money = 0;
+if (money) {
+    console.log("Don't spned it all ;)");
+} else {
+    console.log('You should get a job!');
+}
+
+let height;
+if (height) {
+    console.log("YAY! height is defined!");
+} else {
+    console.log("Height is UNDEFINED");
+}
+
+
+
+//22강
+/*
+const age = '18';
+if (age === 18) console.log('You just became an adult :D (strict)');
+
+if (age == 18) console.log('You just became an adult :D (loose)');
+
+const favorites = Number(prompt("what's your favorite number?"));
+console.log(favorites);
+console.log(typeof favorites);
+
+if (favorites === 23) { // '23' == 23 왼쪽의 조건에서 ==을 쓰느냐 ===을 쓰느냐에 따라 결과값 달라짐
+    console.log('Cool! 23 is an amazing number!')
+} else if (favorites === 7) {
+    console.log('7 is also a cool number')
+} else if (favorites === 9) {
+    console.log('9 is a so-so number')
+} else {
+    console.log('Number is not 23 or 7')
+}
+
+if (favorites !== 23) console.log('why not 23?');
+*/
+
+
+
+
+//23강  : 생략!@! 파이썬/C++에서 다배운것
+
+
+//24강
+
+const hasDriversLicense = true; // A
+const hasGoodVision = true; // B
+
+console.log(hasDriversLicense && hasGoodVision);
+console.log(hasDriversLicense || hasGoodVision);
+console.log(!hasDriversLicense);
+
+const shouldDrive = hasDriversLicense || hasGoodVision;
+
+// if (shouldDrive) {
+//     console.log('Sarah is able to drive!');
+// } else {
+//     console.log('Someone else should drive...');
+// }
+
+
+
+const isTired = false; //C 
+console.log(hasDriversLicense && hasGoodVision && !isTired);
+
+if (hasDriversLicense && hasGoodVision && !isTired) {
+    console.log('Sarah is able to drive!');
+} else {
+    console.log('Someone else should drive...');
+}
