988225557efa2cd5d13f11713aee158490b6c90f
Angela <sonayin61@gmail.com>
2021-05-06 10:27:25 +0900

    YES JAM GAMEMAKING!

--- starter/script9.js
+++
@@ -1,6 +0,0 @@
-let js = 'amazing';
-if (js === 'amazing') alert('JavaScript is FUN!');
-
-40 + 8 + 23 - 10; /* 이렇게 쓰기만해선 어디에도 결과가 안보임 */
-console.log(40 + 8 + 23 - 10); /* 그러나 이처럼 console.log에다 쓰면 브라우저 개발자툴 열었을때 console창에 결과값이 보이게됨 */
-
