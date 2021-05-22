1066e34ed0ba9d34b9bb11e1dc19c8a87cb937ec
Angela <sonayin61@gmail.com>
2021-05-22 23:41:55 +0900

    Jaksim3daysX666

--- script.js
+++ script.js
@@ -1,17 +1,40 @@
+const nums = document.querySelectorAll('.nums span')
+const counter = document.querySelector('.counter')
+const finalMessage = document.querySelector('.final')
+const replay = document.querySelector('#replay')
 
+runAnimation()
 
-const panels = document.querySelectorAll('.panel')
+function resetDOM() {
+  counter.classList.remove('hide')
+  finalMessage.classList.remove('show')
 
-panels.forEach((panel) => {
-    panel.addEventListener('click', () => {
-        removeActiveClasses()
-        panel.classList.add('active')
-    })
+  nums.forEach((num) => {
+    num.classList.value = ''
+  })
 
-})
-
-function removeActiveClasses() {
-    panels.forEach(panel => {
-        panel.classList.remove('active')
-    })
+  nums[0].classList.add('in')
 }
+
+function runAnimation() {
+  nums.forEach((num, idx) => {
+    const nextToLast = nums.length - 1
+
+    num.addEventListener('animationend', (e) => {
+      if (e.animationName === 'goIn' && idx !== nextToLast) {
+        num.classList.remove('in')
+        num.classList.add('out')
+      } else if (e.animationName === 'goOut' && num.nextElementSibling) {
+        num.nextElementSibling.classList.add('in')
+      } else {
+        counter.classList.add('hide')
+        finalMessage.classList.add('show')
+      }
+    })
+  })
+}
+
+replay.addEventListener('click', () => {
+  resetDOM()
+  runAnimation()
+})
